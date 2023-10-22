2b41781336d3a123c6d6cab425185d49b623143e
Roger Reyes <kadoshrr2019@gmail.com>
2023-10-22 16:50:06 -0500

    Added to Get All with Filtering, dynamic segment

--- app.js
+++ app.js
@@ -12,8 +12,15 @@ app.get('/', (req, res) => {
 
 // Get All Movies
 
-app.get('/movies/', (req, res) => {
-    return res.json(movies)
+app.get('/movies', (req, res) => {
+    const { genre } = req.query
+    if (genre) {
+      const filteredMovies = movies.filter(
+        movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+      )
+      return res.json(filteredMovies)
+    }
+    res.json(movies)
 })
 
 // Get an Movie by Id
