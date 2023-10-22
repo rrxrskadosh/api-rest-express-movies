10312d895cd5d9ee04efe5e0f1bc13bcc74e6fde
Roger Reyes <kadoshrr2019@gmail.com>
2023-10-22 16:28:37 -0500

    Fixed typo in get main root

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const movies = require("./movies.json")
 
 const app = express()
 
 app.disabled('x-powered-by')
+app.use(express.json())
 
-app.get('/'), (req, res) => {
+app.get('/', (req, res) => {
     res.json({ message: "Hello server!" })
-}
+})
+
+// Get All Movies
+
+app.get('./movies', (req, res) => {
+    return res.json(movies)
+})
 
 const PORT = process.env.PORT ?? 1234
 
