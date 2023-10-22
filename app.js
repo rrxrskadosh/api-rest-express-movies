e3832f9220840e31422013d653bdce5c693a0aaf
Roger Reyes <kadoshrr2019@gmail.com>
2023-10-22 16:44:48 -0500

    Added Get all movies and Get by ID functions

--- app.js
+++ app.js
@@ -7,17 +7,25 @@ app.disabled('x-powered-by')
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.json({ message: "Hello server!" })
+    res.json({ message: "Hello server API Movies!" })
 })
 
 // Get All Movies
 
-app.get('./movies', (req, res) => {
+app.get('/movies/', (req, res) => {
     return res.json(movies)
 })
 
-const PORT = process.env.PORT ?? 1234
+// Get an Movie by Id
+app.get('/movies/:id', (req, res) => {
+    const { id } = req.params
+    const movie = movies.find(movie => movie.id == id)
+    if(movie) return res.json(movie)
+    res.status(404).json({message: 'Movie not found'})
+})
 
+
+const PORT = process.env.PORT ?? 1234
 app.listen(PORT, () => {
     console.log(`server listen on port http://localhost:${PORT}`)
 })
